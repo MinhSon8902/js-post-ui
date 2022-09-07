baa56ceb8a27edd354d4827b76802f828c51937c
Minh Son <minhson892002@gmail.com>
2022-09-07 16:17:22 +0700

    set up interceptors

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 import axiosClient from './api/axiosClient'
 import postApi from './api/postApi'
 
-console.log('hello')
-
 async function main() {
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 5,
+    }
+    const data = await postApi.getAll(queryParams)
+    console.log(data)
+  } catch (error) {}
   // const response = await axiosClient.get('/posts')
-  const queryParams = {
-    _page: 1,
-    _limit: 5,
-  }
-  const response = await postApi.getAll(queryParams)
-  console.log(response)
 }
 
 main()
